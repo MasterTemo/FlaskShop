84d265edf847e8cefb4ce8afe72e01ad431bd9a0
Temo <50781119+MasterTemo@users.noreply.github.com>
2021-06-04 01:18:52 +0300

    Add files via upload

--- course prj(flask)/main.py
+++ course prj(flask)/main.py
@@ -5,8 +5,10 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import check_password_hash, generate_password_hash #новое
 from cloudipsp import Api, Checkout
 
+
 app = Flask(__name__)
 app.secret_key = 'master temo 1'
+
 
 # настройка конфигурации бд и логин мэнеджера
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
@@ -16,18 +18,19 @@ manager = LoginManager(app)
 manager.login_view = 'login'
 
 
- 
-
-
-
 # Таблица с категориями товаров
 class category(db.Model):
     categid = db.Column(db.Integer, primary_key = True) 
     name = db.Column(db.Text, nullable = False)
     catedesc = db.Column(db.Text, nullable = True)
 
-# Таблица со всеми товарами
+class help(db.Model):
+    id = db.Column(db.Integer, primary_key = True) 
+    name = db.Column(db.Text, nullable = False)
+    Email = db.Column(db.Text, nullable = True)
+    problem = db.Column(db.Text,nullable = False)
 
+# Таблица со всеми товарами
 class Item(db.Model): 
     iditem = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(40), nullable = False)
@@ -36,9 +39,9 @@ class Item(db.Model):
     text = db.Column(db.String(40), nullable = True)
     isActive = db.Column(db.Boolean, default = True)
     category = db.Column(db.Text, nullable = True)
-   
     def __repr__(self):
         return self.title
+
 
 # Таблица со всеми пользователями
 class User(db.Model, UserMixin): 
@@ -49,16 +52,17 @@ class User(db.Model, UserMixin):
     userinfo = db.Column(db.Text, nullable = True)
     #userrole = db.Column(db.Integer, nullable = False) # 1 - админ, 2 - модератор, 3 - обычный пользователь        
 
+
 #Сообщения для форума
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1024), nullable=False)
-
     def __init__(self, text, tags):
         self.text = text.strip()
         self.tags = [
             Tag(text=tag.strip()) for tag in tags.split(',')
         ]
+
 
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -66,15 +70,16 @@ class Tag(db.Model):
     message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
     message = db.relationship('Message', backref=db.backref('tags', lazy=True))
 
+
 class reply(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1024), nullable=False)
-
     def __init__(self, text, tagrep):
         self.text = text.strip()
         self.tagrep = [
             Tag(text=tagtagrep.strip()) for tagtagrep in tagrep.split(',')
         ]
+
 
 class Tagrep(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -88,17 +93,18 @@ class Tagrep(db.Model):
 def load_user(user_id):
     return User.query.get(user_id)
 
+
 #Главная страница
 @app.route('/') 
 def index():
     items = Item.query.order_by(Item.price).all()
     return render_template("base.html", data = items, reply = reply.query.all())
 
+
 #Маленький форум, где люди могут общаться и здававать друг-другу вопросы
 @app.route('/forum', methods=['GET'])
 @login_required
 def forum():
-
     return render_template("forum.html", messages=Message.query.all())
 
 
@@ -108,24 +114,38 @@ def forum():
 def add_reply():
     text = request.form['text']
     tag = request.form['tagrep']
-
     db.session.add(reply(text, tagrep))
     db.session.commit()
-
     return redirect(url_for('/product/<id>'))
 
-#Страница добавления сообщений на мини-форум
 
+#Страница добавления сообщений на мини-форум
 @app.route('/add_message', methods=['POST'])
 @login_required
 def add_message():
     text = request.form['text']
     tag = request.form['tag']
-
     db.session.add(Message(text, tag))
     db.session.commit()
-
     return redirect(url_for('forum'))
+
+
+@app.route('/help', methods = ['POST', 'GET'])
+def helpuser():
+    
+    if request.method == "POST":
+        name = request.form['name']
+        Email = request.form['Email']
+        problem = request.form['problem']
+        me = help(name = name, Email = Email, problem = problem)
+        try:
+            db.session.add(me)
+            db.session.commit()
+            return redirect('/complete')
+        except:
+            return redirect('/complete')
+    return render_template('help.html')
+
 
 #Добавление товаров
 @app.route('/create', methods = ['POST', 'GET']) 
@@ -137,9 +157,7 @@ def create():
         describtion = request.form['describtion']
         text = request.form['text']
         category = request.form['category']
-        
         item = Item(title = title, price = price, describtion = describtion, text = text, category = category)
-
         try:
             db.session.add(item)
             db.session.commit()
@@ -149,11 +167,16 @@ def create():
     return render_template('create.html')
 
 
+@app.route('/complete')
+def complete():
+    return render_template('complete.html')
+
 # Информация о товаре
 @app.route('/product/<id>')
 def product(id):
     desc = Item.query.filter_by(iditem = id)
     return render_template('viewitem.html', data = desc)
+
 
 # Страница юзера
 # Вывод минимальной информации(описание и изображение профиля)
@@ -162,8 +185,8 @@ def product(id):
 def about():
     return render_template('profile.html')
 
+
 # Страница с логином
- 
 @app.route('/login', methods = ['GET','POST']) 
 def login():
     login = request.form.get('login')
@@ -172,18 +195,15 @@ def login():
         user = User.query.filter_by(login=login).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            
             next_page = request.args.get('next')
-            
             return redirect('/')
         else:
             flash('Неправильный логин или пароль')
-            
             return render_template('login.html')
     else:
         flash('Добро пожаловать на наш сайт :)')
-
         return render_template('login.html')
+
 
 #Регистрация пользователя
 #Требуется ввести логин, пароль и подтверждение пароля
@@ -209,6 +229,7 @@ def register():
             return redirect(url_for('login'))
     return render_template('register.html')
 
+
 #Старница редактирования инфы о юзере 
 #Отдельные страницы для редактирования описания профиля и изображения профиля
 @app.route('/editprofile/<id>', methods = ['GET','POST'])
@@ -228,6 +249,7 @@ def editprofile(id):
 
         return render_template('editprofile.html', userinform = userinform)
 
+
 @app.route('/editpic/<id>', methods = ['GET', 'POST'])
 @login_required
 def editpic(id):
@@ -243,6 +265,7 @@ def editpic(id):
             return "Ошибка"
     else:
         return render_template('editpic.html', userpict = userpict)
+
 
 @app.route('/edititem/<id>', methods = ['GET','POST'])
 def edititem(id):
@@ -288,6 +311,7 @@ def logout():
 def support():
     return render_template('support.html')
 
+
 @app.route('/supredirect')
 def supredirect():
     return render_template('supredirect.html')
@@ -309,17 +333,16 @@ def item_buy(id):
     url = checkout.url(data).get('checkout_url')
     return redirect(url)
 
+
 @app.route('/location')
 def location():
     return render_template('location.html')
 
+
 @app.route('/aboutus')
 @login_required
 def aboutus():
-
-
     return render_template('aboutus.html')
-
 
 
 if __name__ == "__main__":
